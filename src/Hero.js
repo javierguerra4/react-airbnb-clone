5e641303d0be8430e315861fb33438f79aa7d582
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-18 15:08:29 -0500

    Created Hero component and added image

---
+++ src/Hero.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Group from "./images/photo-grid.png";
+
+export default function Hero() {
+  return (
+    <div>
+      <img src={Group} alt="Photo grid"></img>
+    </div>
+  );
+}

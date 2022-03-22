c690d430a5c7a629a0adf2833a69cacfdc963369
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-21 22:57:50 -0500

    centered image in div

--- src/Hero.js
+++ src/Hero.js
@@ -3,9 +3,13 @@ import Group from "./images/photo-grid.png";
 
 export default function Hero() {
   return (
-    <div>
-      <img src={Group} alt="Photo grid"></img>
-      <h1> Online Experiences</h1>
+    <div className="hero-container">
+      <img src={Group} alt="Photo grid" className="hero-image"></img>
+      <h1 className="hero-h1"> Online Experiences</h1>
+      <p className="hero-paragraph">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
     </div>
   );
 }

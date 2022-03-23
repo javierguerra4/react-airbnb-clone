de9daa9670ccf8712471b8412491e31ba833d031
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-23 09:33:30 -0500

    cant see star imported image

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Katie from "./images/katie.png";
+import Star from "./images/Star.png";
 
 export default function Card() {
   return (
     <div className="katie-container">
       <img src={Katie} alt="Katie-image" className="katie--image"></img>
+      <div className="rating">
+        <image src={Star} alt="star-image" className="star--image"></image>
+        <p>5.0</p>
+      </div>
       <h1 className="katie-header">Life lessons with Katie Zaferes</h1>
     </div>
   );

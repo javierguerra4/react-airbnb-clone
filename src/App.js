12b3564c62cea9db2e8648e11644eedcb5646dcf
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-22 22:23:33 -0500

    created card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Nav from "./Nav.js";
 import Hero from "./Hero.js";
-
+import Card from "./Card.js";
 export default function App() {
   return (
     <div>
       <Nav />
       <Hero />
+      <Card />
     </div>
   );
 }

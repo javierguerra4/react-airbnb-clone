a905f5024e64f4d6d8caa156ee0a0d357c9acb4c
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-22 22:55:41 -0500

    Updated image and image container styling

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,11 @@
 import React from "react";
+import Katie from "./images/katie.png";
 
 export default function Card() {
-  return <h1>Replace this</h1>;
+  return (
+    <div className="katie-container">
+      <img src={Katie} alt="Katie-image" className="katie--image"></img>
+      <h1 className="katie-header">Life lessons with Katie Zaferes</h1>
+    </div>
+  );
 }

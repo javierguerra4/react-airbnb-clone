8b6bd850450e3e53750faa12788f253b2989f798
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-25 15:08:12 -0500

    Created props for card

--- src/Card.js
+++ src/Card.js
@@ -2,19 +2,21 @@ import React from "react";
 import Katie from "./images/katie.png";
 import Star from "./images/star.png";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="katie-container">
-      <img src={Katie} alt="Katie-image" className="katie--image"></img>
+      <img src={Katie} alt="Katie-image" className="katie--image">
+        {props.img}
+      </img>
       <div className="rating">
         <img src={Star} alt="star-image" className="star--image"></img>
-        <span>5.0</span>
-        <span className="grey">(6) •</span>
-        <span className="grey">USA</span>
+        <span>{rating}</span>
+        <span className="grey">{reviewCount}</span>
+        <span className="grey">{country}</span>
       </div>
-      <p className="katie-header">Life lessons with Katie Zaferes</p>
+      <p className="katie-header">{title}</p>
       <p>
-        <span className="bold">From $136</span> / person
+        <span className="bold">{price}</span> / person
       </p>
     </div>
   );

3bacc5d1b23cd7e4a9b1b2eaabc2de7be513bf15
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-25 15:20:39 -0500

    Able to see card with props, missing image

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import Nav from "./Nav.js";
 import Hero from "./Hero.js";
 import Card from "./Card.js";
+import Star from "./images/star.png";
 export default function App() {
   return (
     <div>
       <Nav />
       <Hero />
-      <Card />
+      <Card
+        img="./images/star.png"
+        rating="5.0"
+        reviewCount="6"
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price="136"
+      />
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -5,18 +5,16 @@ import Star from "./images/star.png";
 export default function Card(props) {
   return (
     <div className="katie-container">
-      <img src={Katie} alt="Katie-image" className="katie--image">
-        {props.img}
-      </img>
+      <img className="katie--image">{props.img}</img>
       <div className="rating">
         <img src={Star} alt="star-image" className="star--image"></img>
-        <span>{rating}</span>
-        <span className="grey">{reviewCount}</span>
-        <span className="grey">{country}</span>
+        <span>{props.rating}</span>
+        <span className="grey">({props.reviewCount})</span>
+        <span className="grey"> ·{props.country}</span>
       </div>
-      <p className="katie-header">{title}</p>
+      <p className="katie-header">{props.title}</p>
       <p>
-        <span className="bold">{price}</span> / person
+        <span className="bold">From ${props.price}</span> / person
       </p>
     </div>
   );

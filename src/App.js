9283628de544e6d241ca0064030f7e3e0cf20cae
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-26 11:18:04 -0500

    Changed reviewCount to a number instead of string

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from "react";
 import Nav from "./Nav.js";
 import Hero from "./Hero.js";
 import Card from "./Card.js";
-import Star from "./images/star.png";
+import Katie from "./images/katie.png";
+
 export default function App() {
   return (
     <div>
       <Nav />
       <Hero />
       <Card
-        // img="./images/star.png"
+        // img="./images/katie.png"
         rating="5.0"
-        reviewCount="6"
+        reviewCount={6}
         country="USA"
         title="Life Lessons with Katie Zaferes"
         price="136"
